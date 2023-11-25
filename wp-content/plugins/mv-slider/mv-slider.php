e3990c4862d0dcbd54d6affd859e20f77b609885
Thiago Campos <=>
2023-11-25 02:56:28 -0300

    add method p remover os registros no deactivate

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -58,6 +58,7 @@ if( ! class_exists("MV_Slider") ){
 
         public static function deactivate(){
             flush_rewrite_rules();
+            unregister_post_type('mv-slider');
         
         }
 
