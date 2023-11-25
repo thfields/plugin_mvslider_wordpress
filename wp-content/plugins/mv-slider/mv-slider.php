2ba2a91e05b8e04cd3e489ad72306b76a0cc4a6e
Thiago Campos <=>
2023-11-25 00:10:37 -0300

    descrições do plugin

---
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -0,0 +1,36 @@
+<?php
+
+
+/**
+ * Plugin Name: MV Slider
+ * Plugin URI: https://github.com/thfields/plugin_mvslider_wordpress
+ * Description: My plugin's description
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Thiago Campos
+ * Author URI: https://github.com/thfields
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mv-slider
+ * Domain Path: /languages
+*/
+
+/*
+MV Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+MV Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MV Slider. If not, see {URI to Plugin License}.
+*/
+
+if( ! defined("ABSPATH") ){
+    die("Não consegue acessar o plugin direto");
+    exit;
+}
