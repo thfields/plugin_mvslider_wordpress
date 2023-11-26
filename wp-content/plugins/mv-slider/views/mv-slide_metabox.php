0d7e21d7c1ac73e6aec045696e791897c2dc7efa
Thiago Campos <=>
2023-11-26 03:48:57 -0300

    API Metabox - Nonces e outras validações2

--- wp-content/plugins/mv-slider/views/mv-slide_metabox.php
+++ wp-content/plugins/mv-slider/views/mv-slide_metabox.php
@@ -1,12 +1,11 @@
 <?php 
-    $meta = get_post_meta( $post->ID);
-    $link_text = get_post_meta( $post->ID,"mv_slider_link_text", true );
-    $link_url = get_post_meta( $post->ID,"mv_slider_link_url", true );
-    
+    $meta = get_post_meta( $post->ID );
+    $link_text = get_post_meta( $post->ID, 'mv_slider_link_text', true );
+    $link_url = get_post_meta( $post->ID, 'mv_slider_link_url', true );
+    //var_dump( $link_text, $link_url );
 ?>
-
 <table class="form-table mv-slider-metabox"> 
-<input type="hidden" name="mv_slider_nonce" value="<?php echo wp_create_nonce("mv_slider_nonce") ?>">
+<input type="hidden" name="mv_slider_nonce" value="<?php echo wp_create_nonce( "mv_slider_nonce" ); ?>">
     <tr>
         <th>
             <label for="mv_slider_link_text">Link Text</label>
@@ -17,7 +16,7 @@
                 name="mv_slider_link_text" 
                 id="mv_slider_link_text" 
                 class="regular-text link-text"
-                value="<?php echo (isset($link_text)) ? esc_html( $link_text ) : ''; ?>"
+                value="<?php echo ( isset( $link_text ) ) ? esc_html( $link_text ) : ''; ?>"
                 required
             >
         </td>
@@ -32,7 +31,7 @@
                 name="mv_slider_link_url" 
                 id="mv_slider_link_url" 
                 class="regular-text link-url"
-                value="<?php echo (isset($link_url)) ? esc_url( $link_url ): ''; ?>"
+                value="<?php echo ( isset( $link_url ) ) ? esc_url( $link_url ) : ''; ?>"
                 required
             >
         </td>
